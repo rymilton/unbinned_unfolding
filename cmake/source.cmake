fb2f58717886c72b6962bfd40bb0d2ba9d6109f6
Will Buttinger <will.buttinger@cern.ch>
2022-06-02 00:30:11 +0200

    Update source.cmake - exploit symlink that cmake process does

--- cmake/source.cmake
+++ cmake/source.cmake
@@ -1,6 +1,6 @@
 file(GLOB RooUnfoldLinkDef RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_HEADER_DIR}/*_LinkDef.h)
 file(GLOB RooUnfoldSources RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_SOURCE_DIR}/*.cxx)
-file(GLOB RooUnfoldHeaders RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_HEADER_DIR}/*.h src/*.tpp)
+file(GLOB RooUnfoldHeaders RELATIVE ${CMAKE_SOURCE_DIR}/src ${ROOUNFOLD_HEADER_DIR}/*.h src/*.tpp)
 list(REMOVE_ITEM RooUnfoldHeaders ${RooUnfoldLinkDef})
 
 file(GLOB RooUnfoldExecSources test/src/RooUnfoldTest.cxx test/src/RooUnfoldTest2D.cxx test/src/RooUnfoldTest3D.cxx)
