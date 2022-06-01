a7898dba0e85c45769fd3f2c353bd79d32591d76
Will Buttinger <will.buttinger@cern.ch>
2022-06-02 00:14:26 +0200

    Update source.cmake - make paths relative so can relocate

--- cmake/source.cmake
+++ cmake/source.cmake
@@ -1,6 +1,6 @@
-file(GLOB RooUnfoldLinkDef ${ROOUNFOLD_HEADER_DIR}/*_LinkDef.h)
-file(GLOB RooUnfoldSources ${ROOUNFOLD_SOURCE_DIR}/*.cxx)
-file(GLOB RooUnfoldHeaders ${ROOUNFOLD_HEADER_DIR}/*.h src/*.tpp)
+file(GLOB RooUnfoldLinkDef RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_HEADER_DIR}/*_LinkDef.h)
+file(GLOB RooUnfoldSources RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_SOURCE_DIR}/*.cxx)
+file(GLOB RooUnfoldHeaders RELATIVE ${CMAKE_SOURCE_DIR} ${ROOUNFOLD_HEADER_DIR}/*.h src/*.tpp)
 list(REMOVE_ITEM RooUnfoldHeaders ${RooUnfoldLinkDef})
 
 file(GLOB RooUnfoldExecSources test/src/RooUnfoldTest.cxx test/src/RooUnfoldTest2D.cxx test/src/RooUnfoldTest3D.cxx)
