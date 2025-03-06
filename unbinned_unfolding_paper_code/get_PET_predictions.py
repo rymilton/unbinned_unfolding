c52cc9e2f34ddd92b1e4d6a466bf2871230ad8e7
rymilton <rmilton@ucla.edu>
2025-03-06 14:27:23 -0800

    Cleaned up arguments section

--- unbinned_unfolding_paper_code/get_PET_predictions.py
+++ unbinned_unfolding_paper_code/get_PET_predictions.py
@@ -7,12 +7,12 @@ import pickle
 import horovod.tensorflow.keras as hvd
 hvd.init()
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Train a PET model using Pythia and Herwig data.")
+    parser = argparse.ArgumentParser(description="Make predictions using trained PET model")
     parser.add_argument("--data_dir", type=str, default="/global/homes/r/rmilton/m3246/rmilton/omnifold_paper_plots/datasets/", help="Folder containing input files")
-    parser.add_argument("--output_dir", type=str, default="/global/homes/r/rmilton/m3246/rmilton/omnifold_paper_plots/unfolding/final_weights", help="Folder to store model weights")
-    parser.add_argument("--save_dir", type=str, default="/global/homes/r/rmilton/m3246/rmilton/omnifold_paper_plots/unfolding/weights/", help="Folder to store trained model weights")
+    parser.add_argument("--output_dir", type=str, default="/global/homes/r/rmilton/m3246/rmilton/omnifold_paper_plots/unfolding/final_weights", help="Folder where trained model is saved")
+    parser.add_argument("--model_dir", type=str, default="/global/homes/r/rmilton/m3246/rmilton/omnifold_paper_plots/unfolding/weights/", help="Folder to store trained model weights")
     parser.add_argument("--num_data", type=int, default=-1, help="Number of data to train with")
-    parser.add_argument("--num_iterations", type=int, default=3, help="Number of iterations to use during training")
+    parser.add_argument("--num_iterations", type=int, default=5, help="Number of iterations to use during training")
     args = parser.parse_args()
     return args
 def expit(x):
@@ -33,7 +33,7 @@ def main():
     synthetic  =  h5.File(synthetic_file_path, 'r')
     synthetic_gen_parts = synthetic['gen'][:num_data]
     model = PET(synthetic_gen_parts.shape[2], num_part=synthetic_gen_parts.shape[1], num_heads = 4, num_transformer = 4, local = True, projection_dim = 128, K = 10)
-    model.load_weights(flags.save_dir + "OmniFold_PET_02_2025_iter4_step2.weights.h5")
+    model.load_weights(flags.model_dir + "OmniFold_PET_02_2025_iter4_step2.weights.h5")
     weights = reweight(synthetic_gen_parts, model)
 
     data_to_save = {
